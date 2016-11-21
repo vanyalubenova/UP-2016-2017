e4057a9ab4e36d0b031eee10e985154f8b926355
vanyalubenova <vanchetolubenova@abv.bg>
2016-11-21 17:03:27 +0200

    Functions

--- Week08 - Functions/Task1.cpp
+++ Week08 - Functions/Task1.cpp
@@ -5,7 +5,29 @@
 #include <iostream>
 
 using namespace std;
+double Min(int a, int b)
+{
+	if (a < b)
+	{
+		b = a;
+	}
+	return b;
+}
+double Pow(double a, int b)
+{
+	double var = 1;
+	for (int i = 0; i < b; i++)
+	{
+		var *= a;
+	}
+	return var;
+}
 
+	for (int i = 0; i < b; i++)
+	{
+		a *= a;
+	}
+}
 int Abs(int n)
 {
 	if (n < 0)
@@ -16,9 +38,6 @@ int Abs(int n)
 }
 int main()
 {
-	int number;
-	cin >> number;
-	cout << Abs(number) << endl;;
 	return 0;
 }
 
