c013253bdebad4bd12358f5ff37e8ed896c8d489
vanyalubenova <vanchetolubenova@abv.bg>
2016-12-18 11:50:30 +0200

    Pointers

--- Week10/Task5.cpp
+++ Week10/Task5.cpp
@@ -5,45 +5,35 @@
 #include <iostream>
 
 using namespace std;
-void toLowerCase(char *string)
-{
-	for (int i = 0; string[i] != 0; i++)
-	{
-		if (string[i] >= 'A' && string[i] <= 'Z')
-		{
-			string[i] = string[i] - 'A' + 'a';
-		}
 
-	}
-}
 void deleteVowels(char *string1, char *string2)
 {
 	int count = 0;
+	int j = 0;
 	for (int i = 0; string1[i] != 0; i++)
 	{
-		if (string1[i] != 'a' && string1[i] != 'e' && string1[i] != 'i' && string1[i] != 'o' && string1[i] != 'u')
+
+		if ((string1[i] >= 'A' && string1[i] <= 'Z') || (string1[i] != 'a' && string1[i] != 'e' && string1[i] != 'i' && string1[i] != 'o' && string1[i] != 'u'))
 		{
-			string2[i] = string1[i];
-			++count;
-		}
+			string2[j] = string1[i];
+			j++;
+			count++;
+		}	
 	}
 	for (int i = 0; i < count; i++)
 	{
 		cout << string2[i];
 	}
-	
+
 
 }
 int main()
 {
 	char string1[100];
 	cin >> string1;
-	toLowerCase(string1);
 	char string2[100];
-	
 	deleteVowels(string1, string2);
-	
-	
+
+
 	return 0;
 }
-
