a952ca2c9edb7a42a803e476f1253d8c7bc3b8e3
vanyalubenova <vanchetolubenova@abv.bg>
2016-12-18 12:33:02 +0200

    Pointers

--- Week10/Task6.cpp
+++ Week10/Task6.cpp
@@ -15,27 +15,24 @@ void initArray(double *array, int n)
 }
 double lastAverage(double *array, int n)
 {
-	int last = INT_MIN;
+	int last = -1;
 	for (int i = 0; i < n; i++)
 	{
 		if (i == 0 || i == n - 1)
 		{
-			if (array[0] == array[1])
+			if (array[0] == array[1] || array[n - 2] == array[n - 1])
 			{
-				last = array[0];
+				last = i;
 			}
-			if (array[n - 2] == array[n - 1])
-			{
-				last = array[n - 1];
-			}
+			
 		}
 		else if(array[i] == ((array[i - 1] + array[i + 1]) / 2.0))
 		{
-			last = array[i];
+			last = i;
 		}
 		
 	}
-	if (last)
+	if (last != - 1)
 	{
 		return last;
 	}
