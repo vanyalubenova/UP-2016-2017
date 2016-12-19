4c83fd223b05100f0ec0514b10bf385b7555ca0e
vanyalubenova <vanchetolubenova@abv.bg>
2016-12-19 17:00:50 +0200

    Pointers

--- Week10/Task5.cpp
+++ Week10/Task5.cpp
@@ -10,11 +10,11 @@ void deleteVowels(char *string)
 {
 	int count1 = 0;
 	int count2 = 0;
-	while (string[count1]!= 0)
+	while (string[count1]!= '\0')
 	{
 		count1++;
 	}
-	for (int i = 0; string[i]!= 0; i++)
+	for (int i = 0; string[i]!= '\0'; i++)
 	{
 	
 		if (string[i] == 'a' || string[i] == 'e' || string[i] == 'i' || string[i] == 'o' || string[i] == 'u')
